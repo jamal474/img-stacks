581accff73ddffc2c8374ec4a287eee4d73b3abc
Andy Hoffman <antibland@gmail.com>
2025-01-06 21:54:09 -0500

    chore: upgrade build details

--- _build_npm.ts
+++ _build_npm.ts
@@ -1,4 +1,5 @@
 #!/usr/bin/env -S deno run --allow-read --allow-write --allow-net --allow-env --allow-run
+/// <reference lib="deno.ns" />
 
 import { build, emptyDir } from "https://deno.land/x/dnt@0.38.1/mod.ts";
 
@@ -6,42 +7,39 @@ async function start() {
   await emptyDir("./npm");
 
   await build({
-    entryPoints: ["./mod.tsx"],
+    entryPoints: ["./main.ts"],
     outDir: "./npm",
     shims: {
       deno: true,
     },
-    test: false,
-    typeCheck: "both",
-    compilerOptions: {
-      importHelpers: false,
-      sourceMap: true,
-      target: "ES2021",
-      lib: ["esnext", "dom", "dom.iterable"],
-      jsx: "react-jsx",
-    },
     package: {
       name: "img-stacks",
-      version: Deno.args[0],
-      description:
-        "A React component for displaying stacked images with interactive features",
+      version: "0.1.0",
+      description: "Beautiful, interactive image stacks for React",
       license: "MIT",
-      keywords: ["react", "images", "gallery", "stack", "animation"],
-      engines: {
-        node: ">=14.0.0",
-      },
       repository: {
         type: "git",
-        url: "git+https://github.com/antibland/img-stacks.git",
+        url: "git+https://github.com/antibland/npm-img-stacks.git",
       },
       bugs: {
-        url: "https://github.com/antibland/img-stacks/issues",
+        url: "https://github.com/antibland/npm-img-stacks/issues",
+      },
+      dependencies: {
+        react: "^18.2.0",
+        "react-dom": "^18.2.0",
+      },
+      devDependencies: {
+        "@types/react": "^18.2.0",
+        "@types/react-dom": "^18.2.0",
+        typescript: "^5.0.0",
       },
       peerDependencies: {
-        react: "^17.0.0 || ^18.0.0",
-        "react-dom": "^17.0.0 || ^18.0.0",
+        react: "^18.2.0",
+        "react-dom": "^18.2.0",
       },
     },
+    typeCheck: false,
+    test: false,
   });
 
   await Deno.copyFile("LICENSE", "npm/LICENSE");
